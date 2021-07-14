71501d7323040d74acc7b7f2fdf0b32db7f2025c
XXX70000G <sean.van@foxmail.com>
2021-07-14 20:16:42 +0800

    分支测试

--- src/main/java/com/example/demogit/Test.java
+++ src/main/java/com/example/demogit/Test.java
@@ -1,8 +1,54 @@
 package com.example.demogit;
 
 public class Test {
+
     private String name;
 
     private String age;
-    private String seck;
+
+    private String sex;
+
+    private String haha;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getHaha() {
+        return haha;
+    }
+
+    public void setHaha(String haha) {
+        this.haha = haha;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", sex='" + sex + '\'' +
+                ", haha='" + haha + '\'' +
+                '}';
+    }
 }
