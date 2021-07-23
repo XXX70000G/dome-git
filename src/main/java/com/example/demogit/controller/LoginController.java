7cbb5cf7e09ca45647ea372fbbd32ab5b8c9c1bf
XXX70000G <sean.van@foxmail.com>
2021-07-23 22:04:12 +0800

    测试stash命令，模拟创建Bug分支，修复Bug后，合并到master分支。

---
+++ src/main/java/com/example/demogit/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.example.demogit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "Hello Word";
+    }
+
+    @GetMapping("/logout")
+    public String login(String a){
+        return "Hello Word";
+    }
+}
