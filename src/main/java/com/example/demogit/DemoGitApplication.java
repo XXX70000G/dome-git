0ff088f12496cefb2623f88600f1e1380fb06372
XXX70000G <sean.van@foxmail.com>
2021-07-23 21:23:15 +0800

    测试分支管理策略

--- src/main/java/com/example/demogit/DemoGitApplication.java
+++ src/main/java/com/example/demogit/DemoGitApplication.java
@@ -2,8 +2,10 @@ package com.example.demogit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class DemoGitApplication {
 
     public static void main(String[] args) {
